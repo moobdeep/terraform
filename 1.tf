dd691842dcd08a66ebcef7005e1d0e7a6f515d80
moobdeep <moobdeep@bigmir.net/>
2019-10-22 18:25:42 +0300

    firs commit from console

--- 1.tf
+++ 1.tf
@@ -1,1 +1,8 @@
-1
+resource "aws_instance" "example" {
+  ami           = "ami-03b4b78aae82b30f1"
+  instance_type = "t2.micro"
+
+  provisioner "local-exec" {
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,6 @@
+provider "aws" {
+  region = "eu-west-3"
+  access_key = "AKIA4YQAWYC5QDNOBFUI"
+  secret_key = "L9jLPKiWxvHlziNVGm7pGYYr6C3mEJeLQodtGfo7"
+}
+
